cd2de5c1be14865941cf612d05f05f91dc2686be
youjin <howyoujini@gmail.com>
2022-07-26 00:12:27 +0900

    Add: duck.dart

--- dartLang/Bicycle.java
+++ dartLang/Bicycle.java
@@ -1,5 +1,14 @@
 package dartLang;
 
+oop : 외부로부터 변수값에 직접적으로 접근하는 것을 막고 있다.
+직접 접근하면 무결성이 깨질 수 있기 때문에 
+
+함수를 통해 값을 전달 받고, 전달하는 방식을 권장
+class 내에서는 변수는 private(캡슐화, 정보은닉)로 선언해서 외부에서 바라볼 수 없도록 해야함.
+
+SET 은 변수값을 할당하는 목적의 함수이기 때문에 인자를 받아야 하고
+GET 은 변수값을 반환하는 목적이기 때문에 return 이 필요히다.
+
 public class Bicycle {
     private int cadence;
     private int speed;
---
+++ simUDuck/duck.dart
@@ -0,0 +1,20 @@
+class Duck {
+  quack() {
+    // TODO: implement quack
+    throw UnimplementedError();
+  }
+swim() {
+  // TODO: implement swim
+  throw UnimplementedError();
+}
+display() {
+  // TODO: implement display
+  throw UnimplementedError();
+}
+
+}
+
+void main(){
+  final duck = Duck();
+  duck;
+}
